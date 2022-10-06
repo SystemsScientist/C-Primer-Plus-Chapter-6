7dd32641211bbab2ada507621739761453ce1811
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-06 06:47:42 -0500

    Adding Listing-6-2

---
+++ Listing-6-2/when.c
@@ -0,0 +1,18 @@
+// when.c -- finishes when a loop quits
+
+#include <stdio.h>
+
+int main() {
+
+	int n = 5;
+
+	while (n < 7) {
+
+		printf("n = %d\n", n);
+		n++;
+		printf("Now n = %d\n", n);
+	}
+	printf("The loop has finished.\n");
+
+	return 0;
+}
