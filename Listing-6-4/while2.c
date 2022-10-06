7e233b3ed5a20abba756af140662f66fe7377c73
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-06 07:03:51 -0500

    Adding Listing-6-4

---
+++ Listing-6-4/while2.c
@@ -0,0 +1,14 @@
+// while2.c -- watch your semicolons
+
+#include <stdio.h>
+
+int main() {
+
+	int n = 0;
+
+	while (n++ < 3);
+		printf("n is %d\n", n);
+	printf("That's all this program does.\n");
+
+	return 0;
+}
