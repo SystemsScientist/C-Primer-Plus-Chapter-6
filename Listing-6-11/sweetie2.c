f060a18cfcab4c7d1847dafb5d6a2612df2e495d
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-07 06:58:07 -0500

    Adding Listing-6-11

---
+++ Listing-6-11/sweetie2.c
@@ -0,0 +1,14 @@
+// sweetie2.c -- a counting loop using for
+
+#include <stdio.h>
+
+int main() {
+
+	const int NUMBER = 22;
+	int count;
+
+	for (count = 1; count <= NUMBER; count++)
+		printf("Be my Valentine!\n");
+
+	return 0;
+}
