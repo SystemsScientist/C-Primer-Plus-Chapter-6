061b3ba2a2080c42bf25d11ff3c8fad1d56bcf91
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-07 07:48:19 -0500

    Adding for_geo.c file

---
+++ for_geo.c
@@ -0,0 +1,13 @@
+// for_geo.c
+
+#include <stdio.h>
+
+int main() {
+
+	double debt;
+
+	for (debt = 100.0; debt < 150.0; debt = debt * 1.1)
+		printf("Your debt is now $%.2f.\n", debt);
+
+	return 0;
+}
