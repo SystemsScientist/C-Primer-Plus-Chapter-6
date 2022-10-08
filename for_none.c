75af5c81425af1ffe3199e3175427888aa8d6ebd
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-08 06:22:48 -0500

    Add for_none.c file

---
+++ for_none.c
@@ -0,0 +1,16 @@
+// for_none.c
+
+#include <stdio.h>
+
+int main() {
+
+	int ans, n;
+
+	ans = 2;
+	for (n = 3; ans <= 25; )
+		ans = ans * n;
+	
+	printf("n = %d; ans = %d.\n", n, ans);
+
+	return 0;
+}
