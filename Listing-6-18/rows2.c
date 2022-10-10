bc92ed5b3d5f51787268130b6446365b50db2450
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-10 06:22:03 -0500

    Adding Listing-6-18

---
+++ Listing-6-18/rows2.c
@@ -0,0 +1,20 @@
+// rows2.c -- using dependent nested rows
+
+#include <stdio.h>
+
+int main() {
+
+	const int ROWS = 6;
+	const int CHARS = 6;
+
+	int row;
+	char ch;
+
+	for (row = 0; row < ROWS; row++) {
+		for (ch = ('A' + row); ch < ('A' + CHARS); ch++)
+			printf("%c", ch);
+		printf("\n");
+	}
+
+	return 0;
+}
